cd3a5f5a7f5bf1bf35622add8c9c9234216c9a48
Dongfang Zhao <dzhao@uw.edu>
2023-08-05 07:56:00 +0000

    fix default tabsize

--- Assignment_4/NamedAccount.java
+++ Assignment_4/NamedAccount.java
@@ -8,16 +8,18 @@
 */
 
 /**
-* Please see BankAccount.java by ian skyles for more info
-* @author Ian Skyles iskyles@uw.edu
-* @version 10/21/2014
-*/
-public interface NamedAccount{
+ * Please see BankAccount.java by ian skyles for more info
+ * 
+ * @author Ian Skyles iskyles@uw.edu
+ * @version 10/21/2014
+ */
+public interface NamedAccount {
 
-   /*
-    *defines the methods required by anything within interface
-    */  
-   
-    String getAccountHolderName();
-    void setAccountHolderName(final String newName);
- }
+	/*
+	 * defines the methods required by anything within interface
+	 */
+
+	String getAccountHolderName();
+
+	void setAccountHolderName(final String newName);
+}
